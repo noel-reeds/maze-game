c424c516b5f8b0aa1063ed9bf2a6f872bfdcdcb4
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-26 10:59:59 +0300

    Main file

--- src/main.c
+++ src/main.c
@@ -1,25 +1,27 @@
-#include <SDL2/SDL.h>
-#include "main.h"
+#include "inc/main.h"
 /**
  * main - entry point of a C program
- * Return: returns void
+ * Return: returns 0 on success
  */
 int main(void)
 {
-    SDL_Instance instance;
-    if (init_instance(&instance) != 0)
-        return (1);
-    while ("C is awesome")
-    {
-        SDL_SetRenderDrawColor(instance.renderer, 0, 0, 255, 255);
-        SDL_RenderClear(instance.renderer);
-		if (poll_events() == 1)
-			break;
-		draw_stuff(instance);
-        SDL_RenderPresent(instance.renderer);
-    }
-	SDL_DestroyRenderer(instance.renderer);
-	SDL_DestroyWindow(instance.window);
-	SDL_Quit();
-    return (0);
+        SDL_Instance instance;
+
+        if (init_instance(&instance) != 0)
+                return (1);
+
+        while ("Battle Snipper")
+        {
+                SDL_SetRenderDrawColor(instance.renderer,  0, 0, 255, 255);
+                SDL_RenderClear(instance.renderer);
+                if (poll_events() == 1)
+                        break;
+                draw_stuff(instance);
+                SDL_RenderPresent(instance.renderer);
+        }
+
+        SDL_DestroyRenderer(instance.renderer);
+        SDL_DestroyWindow(instance.window);
+        SDL_Quit();
+        return (0);
 } 

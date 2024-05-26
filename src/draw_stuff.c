f0fe7b6dc79ab83b362a2a6d0c6ae0fdf279368b
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-26 18:08:03 +0300

    draw map

--- src/draw_stuff.c
+++ src/draw_stuff.c
@@ -6,15 +6,35 @@
  *
  * Return: returns void
  */
-void draw_stuff(SDL_Instance instance)
+int map[MAP_SIZE][MAP_SIZE] = {
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 0, 1, 1, 0, 1},
+        {1, 0, 1, 0, 0, 1, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 0, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+};
+
+//Player player = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0};
+
+void draw_map(SDL_Instance *instance)
 {
-	SDL_Point cordinates[] = {
-		{50, 50},
-		{200, 200},
-		{300, 100},
-		{450, 200}
-	};
-	int numpoints = sizeof(cordinates) / sizeof(cordinates[0]);
-	SDL_SetRenderDrawColor(instance.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-	SDL_RenderDrawLines(instance.renderer, cordinates, numpoints);
+        int x, y;
+
+        for (y = 0; y < MAP_SIZE; y++)
+        {
+                for (x = 0; x < MAP_SIZE; x++)
+                {
+                        if (map[y][x] == 1)
+                        {
+                                SDL_Rect wall = {x * TILE_SIZE, y * TILE_SIZE,
+                                        TILE_SIZE, TILE_SIZE};
+                                SDL_SetRenderDrawColor(
+                                        instance->renderer, 200, 200, 200, 255);
+                                SDL_RenderFillRect(instance->renderer, &wall);
+                        }
+                }
+        }
 }

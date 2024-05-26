19f4155881cef16f8734b78471fb24b51a4b7fae
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-26 11:01:11 +0300

    create window and render on it

--- src/render.c
+++ src/render.c
@@ -1,8 +1,7 @@
-#include <SDL2/SDL.h>
-#include "main.h"
+#include "inc/main.h"
 /**
  * init_instance - initializes an instance
- *@instance: SDL Instance
+ * @instance: SDL Instance
  * Return: returns 0 for success, 1 for an err.
  */
 int init_instance(SDL_Instance *instance)
@@ -10,22 +9,27 @@ int init_instance(SDL_Instance *instance)
 	/* Initializes an SDL instance */
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
-		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+		printf("Error initializing SDL: %s\n", SDL_GetError());
 		return (1);
 	}
+
 	/* create a new window instance */
-	instance->window = SDL_CreateWindow("Battlefield Snipper", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1260, 720, 0);
+	instance->window = SDL_CreateWindow("Battle Snipper",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1260, 720, 0);
+
 	if (instance->window == NULL)
 	{
-		fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+		printf("Error creating window: %s\n", SDL_GetError());
 		SDL_Quit();
 		return (1);
 	}
-	instance->renderer = SDL_CreateRenderer(instance->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	instance->renderer = SDL_CreateRenderer(instance->window, -1,
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (instance->renderer == NULL)
 	{
 		SDL_DestroyWindow(instance->window);
-		fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+		printf("Error creating renderer: %s\n", SDL_GetError());
 		SDL_Quit();
 		return (1);
 	}

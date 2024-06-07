4417962c56f869f254346afdec6772198b9b1778
altoid <noelreeds01@gmail.com>
2024-06-07 23:12:28 +0000

    betty coding style validated

--- src/player.c
+++ src/player.c
@@ -1,15 +1,18 @@
 #include "../inc/main.h"
 /**
- * draw_stuff - render a random stuff
+ * create_player - creates a player in a field
  *
  *@instance: SDL instance.
+ *
+ *@player: a player in a battle.
  *
  * Return: returns void
  */
 
 void create_player(SDL_Instance *instance, Player *player)
-{ 
+{
 	SDL_Rect player_rect = {player->px, player->py, PLAYER_SIZE, PLAYER_SIZE};
+
 	SDL_SetRenderDrawColor(instance->renderer, 0, 0, 255, 0);
 	SDL_RenderFillRect(instance->renderer, &player_rect);
 }

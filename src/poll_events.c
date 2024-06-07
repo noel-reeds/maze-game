f9f3e7ee00bd5a617f3473f838f72fb4b571d41a
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-06-07 15:52:33 +0300

    Events to move a player

--- src/poll_events.c
+++ src/poll_events.c
@@ -1,26 +1,49 @@
-#include "inc/main.h"
+#include "../inc/main.h"
 /**
- * poll_events - retrieves pending events.
- *
+ * poll_events - handles user input through event loop from the SDL event queue
+ * @player: pointer that access players attributes on Player struct
  * Return: returns 0 on success, otherwise err.
  */
-int poll_events()
+int poll_events(Player *player)
 {
 	SDL_Event event;
-	SDL_KeyboardEvent key;
+	int new_px, new_py;
+	float angle;
 
 	while (SDL_PollEvent(&event))
 	{
-		switch (event.type)
+		if (event.type == SDL_QUIT)
 		{
-		case SDL_QUIT:
-			return (1);
-		case SDL_KEYDOWN:
-			key = event.key;
-			if (key.keysym.scancode == 0x29)
+			return (1); }
+		if (event.type == SDL_KEYDOWN)
+		{
+			new_px = player->px;
+			new_py = player->py;
+			angle = player->pa;
+
+			if (event.key.keysym.sym == 'w')
+				new_py -= PLAYER_SPEED;
+			else if (event.key.keysym.sym == 's')
+				new_py += PLAYER_SPEED;
+			else if (event.key.keysym.sym == 'a')
+				new_px -= PLAYER_SPEED;
+			else if (event.key.keysym.sym == 'd')
+				new_px += PLAYER_SPEED;
+			else if (event.key.keysym.sym == 'q')
+				angle -= 1;
+			else if (event.key.keysym.sym == 'e')
+				angle += 1;
+			else if (event.key.keysym.sym == 0x1B)
 				return (1);
-			break;
+
+			if (!collision_detection(new_px, new_py))
+			{
+				player->px = new_px;
+				player->py = new_py;
+				player->pa = angle;
+			}
 		}
 	}
+
 	return (0);
 }

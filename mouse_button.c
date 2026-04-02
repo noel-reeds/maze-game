1a7de04b301bd8ab39957d6844ab74b8688cc8b4
altoids <noelreeds01@gmail.com>
2026-04-02 16:30:44 +0300

    defines mouse events functions

--- mouse_button.c
+++ mouse_button.c
@@ -1,22 +1,18 @@
 #include "main.h"
-enum ButtonSprite current_sprite;
-SDL_Surface *ButtonSprite[ TOTAL_BUTTONS ];
-SDL_Point pos;
-
-void initialize_button(void)
+void initialize_button(btt_e_handlr *self)
 {
-	pos.x = 0;
-	pos.y = 0;
-	current_sprite = BS_MOUSE_OUT;
+	self->pos.x = 0;
+	self->pos.y = 0;
+	self->current_sprite = BS_MOUSE_OUT;
 }
 
-void set_button_position(int x, int y)
+void set_button_position(btt_e_handlr *self, int x, int y)
 {
-	pos.x = x;
-	pos.y = y;
+	self->pos.x = x;
+	self->pos.y = y;
 }
 
-void handle_event(SDL_Event *event)
+void handle_event(btt_e_handlr *self, SDL_Event *event)
 {
 	if (event->type == SDL_MOUSEMOTION || event->type == SDL_MOUSEBUTTONDOWN ||	event->type == SDL_MOUSEBUTTONUP)
 	{
@@ -25,33 +21,33 @@ void handle_event(SDL_Event *event)
 		bool inside = true;
 
 		/* check if mouse is outside a button or not*/
-		if (x < pos.x || x > pos.x + BUTTON_WIDTH)
+		if (x < self->pos.x || x > self->pos.x + BUTTON_WIDTH)
 			inside = false;
-		else if (y < pos.y || y > pos.y + BUTTON_HEIGHT)
+		else if (y < self->pos.y || y > self->pos.y + BUTTON_HEIGHT)
 			inside = false;
 		if (!inside)
-			current_sprite = BS_MOUSE_OUT;
+			self->current_sprite = BS_MOUSE_OUT;
 		else
 		{
 			switch(event->type)
 			{
 				case SDL_MOUSEMOTION:
-				current_sprite = BS_MOUSE_OVER_MOTION;
+				self->current_sprite = BS_MOUSE_OVER_MOTION;
 				break;
 				
 				case SDL_MOUSEBUTTONDOWN:
-				current_sprite = BS_MOUSE_DOWN;
+				self->current_sprite = BS_MOUSE_DOWN;
 				break;
 
 				case SDL_MOUSEBUTTONUP:
-				current_sprite = BS_MOUSE_UP;
+				self->current_sprite = BS_MOUSE_UP;
 				break;
 			}
 		}
 	}
 }
 
-void button_render(void)
+void button_render(btt_e_handlr *self)
 {
-	render(renderer, ss_texture, pos.x, pos.y, sprite_clips[current_sprite], 0.0, NULL, SDL_FLIP_NONE);
+	render(renderer, ss_texture, self->pos.x, self->pos.y, sprite_clips[self->current_sprite], 0.0, NULL, SDL_FLIP_NONE);
 }

a70f0fec7f0117e92c006a43afe0263619739398
altoids <noelreeds01@gmail.com>
2026-05-14 17:36:26 +0300

    uses WSAD for player movement

--- handle_event.c
+++ handle_event.c
@@ -3,54 +3,46 @@
 
 void handle_keyboard_event(collision_handlr *self, SDL_Event event_e)
 {
-	if (event_e.type == SDL_KEYDOWN)
+	if (event_e.type == SDL_KEYDOWN && event_e.key.repeat == 0)
 	{
 		switch(event_e.key.keysym.sym)
 		{
 			case SDLK_w:
-			self->vel_y -= self->p8_velocity; 
+			self->vel_x -= self->p8_velocity;
 			break;
 
 			case SDLK_s:
-			self->vel_y += self->p8_velocity;
-			break;
-
-			case SDLK_d:
 			self->vel_x += self->p8_velocity;
 			break;
 
 			case SDLK_a:
-			self->vel_x -= self->p8_velocity;
-			break;
-		}
-	}
-	if (event_e.type == SDL_KEYUP)
-	{
-		switch(event_e.key.keysym.sym)
-		{
-			case SDLK_w:
 			self->vel_y -= self->p8_velocity;
 			break;
 
+			case SDLK_d:
+			self->vel_y += self->p8_velocity;
+			break;
+		}
+	}
+	else if (event_e.type == SDL_KEYUP && event_e.key.repeat == 0)
+	{
+		switch(event_e.key.keysym.sym)
+		{
+			case SDLK_w:
+			self->vel_x += self->p8_velocity;
+			break;
+
 			case SDLK_s:
+			self->vel_x -= self->p8_velocity;
+			break;
+
+			case SDLK_a:
 			self->vel_y += self->p8_velocity;
 			break;
 
 			case SDLK_d:
-			self->vel_x += self->p8_velocity;
-			break;
-
-			case SDLK_a:
-			self->vel_x -= self->p8_velocity;
+			self->vel_y -= self->p8_velocity;
 			break;
 		}
 	}
-	if (self->pos_x > SCREEN_WIDTH - self->p8_width)
-        self->pos_x = SCREEN_WIDTH - self->p8_width;
-    if (self->pos_y > SCREEN_HEIGHT - self->p8_width)
-        self->pos_y = SCREEN_HEIGHT - self->p8_width;
-    if (self->pos_y < 0)
-        self->pos_y = 0;
-    if (self->pos_x < 0)
-        self->pos_x = 0;
 }
